09ffbce4b14696fafbf5240917e5926e57827b85
PavloM19 <mpa_dn@ua.fm>
2023-07-08 14:20:21 +0300

    Підкоректував з українською локалізацією
    1. Імена а призвища стали украінські
    2. спростив генерацію номеру телефона + додав коди опеаторів сотового зв'язку
    3. вік людей став від 21 до 50 років
    4. для мейлів зробив транслітерацію імен
    5 . Адреса теперь з містом (раніше була тільки вулиця)

--- write_read_file.py
+++ write_read_file.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from collections import UserDict
 import csv
 from datetime import datetime
@@ -64,24 +63,24 @@ class Birthday(Field):
     def __str__(self):
         return self.data_str
 
-class Record:
-    def __init__(self, name: Name, phone=None, birthday=None, email=None): 
-        self.name = name
-        self.email = email
-        self.phones = []
-        
-        if phone:
-            if type(phone) is Phone:                
-                if phone.value:
-                    self.phones.append(phone)
-            else:
-                print('Error. phone must be an object of class Phone!')
+    
+class Address(Field):
+    def __init__(self, name):
+        super().__init__(name)   
 
-        if birthday:
-            if type(birthday) is Birthday:
-                self.birthday = birthday
-            else:
-                print('Error, birthday must be an object of class Birthday!')
+
+class Record:
+    def __init__(self, name: Name, phone=None, birthday=None, email=None, address=None): 
+        self.name = name
+        self.phones = []
+        self.birthday = birthday
+        self.email = email
+        self.address = address
+        
+        if phone:            
+            self.phones.append(phone)
+
+
 
     def __str__(self):
         return f'# {self.name}: {self.phones} ({self.birthday})' # '#' - selector
@@ -154,145 +153,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-from collections import UserDict
-import csv
-from datetime import datetime
-from pathlib import Path
-import re
-
-
-class Field:
-    def __init__(self, value):
-        self.__value = None
-        self.value = value
-
-    def __str__(self):
-        return str(self.__value)
-
-    def __repr__(self):
-        return self.__str__()    
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
-        self.__value = value
-
-
-class Name(Field):
-    def __init__(self, name):
-        super().__init__(name)
-    
-
-class Phone(Field):
-    def __init__(self, phone):
-        if phone:
-            if phone.isdigit():
-                self.phone = phone
-            else:
-                self.phone = None
-                print('Error, phone must contain only digits')
-        super().__init__(phone)
-
-
-class Email(Field):
-    def __init__(self, name):
-        super().__init__(name)   
-
-
-class Birthday(Field):
-    def __init__(self, birthday: str):  #format: 'dd.mm.YYYY or dd/mm/YYYY or dd-mm-YYYY'
-        if birthday:
-            self.data_str = re.sub(r'[/-]', '.', birthday.strip())
-            try:
-                data = datetime.strptime(self.data_str, '%d.%m.%Y')
-            except ValueError as err:
-                print('bot>> Error, ' + str(err))
-            else:
-                self.data = data 
-        else:
-            self.data_str = ''
-
-    def __str__(self):
-        return self.data_str
-
-class Record:
-    def __init__(self, name: Name, phone=None, birthday=None, email=None): 
-        self.name = name
-        self.email = email
-        self.phones = []
-        
-        if phone:
-            if type(phone) is Phone:                
-                if phone.value:
-                    self.phones.append(phone)
-            else:
-                print('Error. phone must be an object of class Phone!')
-
-        if birthday:
-            if type(birthday) is Birthday:
-                self.birthday = birthday
-            else:
-                print('Error, birthday must be an object of class Birthday!')
-
-    def __str__(self):
-        return f'# {self.name}: {self.phones} ({self.birthday})' # '#' - selector
-
-    def __repr__(self):
-        return self.__str__()
-    
-    def add_new_phone(self, new_phone):
-        if type(new_phone) is Phone:
-            self.phones.append(new_phone)
-        else:
-            self.phones.append(Phone(new_phone))
-
-    def del_phone(self, phone=''):
-        if not phone:
-            self.phones.pop()  #delete the last phone number
-        else:
-            phone = phone.strip()
-            if phone in self.phones:
-                self.phones.remove(phone)
-                return 'Phone number was deleted'
-            else:
-                return 'Error. This number is not in the phone list'
-
-
-
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def del_record(self, name):
-        if name not in self.data:
-            print(f'bot>> Record with the name {name} not found')
-        else:
-            self.data.pop(name)
-
-
-
-# *********************************************************************************************************
-
-
-def write_AB(path, adr_book):    #write AddressBook in file
-    if not len(adr_book):
-        print('AddressBook is empty!')
-    else:
-        field_names = list(adr_book.data[list(adr_book.data)[0]].__dict__) # if type(adr_book) = class AddressBook
-        # field_names = list(adr_book[list(adr_book)[0]].__dict__) # if type(adr_book) = dict
-        with open(path, 'w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=field_names)
-            writer.writeheader()
-            for rec in adr_book.values():
-                writer.writerow(rec.__dict__)
-
-
-
-if __name__ == "__main__":
-    path_file = Path(__file__).parent / 'AddressBook.csv'
-    
->>>>>>> Stashed changes
