ec466a37b7e19b068fe1d3762e4862b669884aeb
UstymenkoOB <50136631+UstymenkoOB@users.noreply.github.com>
2023-07-09 11:24:56 +0100

    Interface_2
    Додано пункт "Інші"

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ from tkinter import *
 import addressbook
 import notebook
 import sort
+import other
 
 def addressbook_but():
     addressbook.main()
@@ -11,6 +12,9 @@ def notebook_but():
 
 def sorter_but():
     sort.main()
+
+def other_but():
+    other.main()
 
 def exit_but():
     tk.destroy()
@@ -32,13 +36,15 @@ def main():
     but_1 = Button(tk, text="Адресна книга", command=addressbook_but, font="Arial 15", width=18)
     but_2 = Button(tk, text="Нотатник", command=notebook_but, font="Arial 15", width=18)
     but_3 = Button(tk, text="Сортувальник", command=sorter_but, font="Arial 15", width=18)
-    but_4 = Button(tk, text="Вихід", command=exit_but, font="Arial 15", width=18)
+    but_4 = Button(tk, text="Інше", command=other_but, font="Arial 15", width=18)
+    but_5 = Button(tk, text="Вихід", command=exit_but, font="Arial 15", width=18)
     footer = Label(tk, text="© Dream Team", bg="grey", font='Arial 10', foreground="white")
     tit.place(x=30, y=50)
     but_1.place(x=150, y=150)
     but_2.place(x=150, y=200)
     but_3.place(x=150, y=250)
     but_4.place(x=150, y=300)
+    but_5.place(x=150, y=350)
     footer.place(x=20, y=475)
     tk.mainloop()
 
